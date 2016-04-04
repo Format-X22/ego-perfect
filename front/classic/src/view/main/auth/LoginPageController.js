cae48d81b0e095a02139a9cd0140a224f4a0300f
Pavlov <oleg.pav.m@gmail.com>
2016-04-04 21:20:14 +0300

    Вью сброса пароля.

--- front/classic/src/view/main/auth/LoginPageController.js
+++ front/classic/src/view/main/auth/LoginPageController.js
@@ -5,5 +5,30 @@ Ext.define('A.view.main.auth.LoginPageController', {
     extend: 'A.view.main.auth.AbstractAuthController',
     alias: 'controller.mainAuthLoginPage',
 
-    url: '/api/auth/login'
+    url: '/api/auth/login',
+
+    config: {
+        restorePassUrl: '/api/auth/restorePass'
+    },
+
+    /**
+     * Сбрасывает пароль.
+     */
+    resetPass: function () {
+        var view = this.getView();
+
+        view.mask();
+        view.submit({
+            clientValidation: true,
+            url: this.getRestorePassUrl(),
+            success: function () {
+                this.showSuccessMessage('Новый пароль отправлен вам на почту.');
+                view.unmask();
+            }.bind(this),
+            failure: function () {
+                this.showFailureSaveMessage.apply(this, arguments);
+                view.unmask();
+            }.bind(this)
+        });
+    }
 });
