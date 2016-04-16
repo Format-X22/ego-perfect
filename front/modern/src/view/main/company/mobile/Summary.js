9f09b015b5a26978a391e8cb6f0acd19b3921db7
Pavlov <oleg.pav.m@gmail.com>
2016-04-16 16:05:24 +0300

    Фикс бага с  jpEg, вывода рейтинга и отступы на мобильных.

--- front/modern/src/view/main/company/mobile/Summary.js
+++ front/modern/src/view/main/company/mobile/Summary.js
@@ -20,7 +20,7 @@ Ext.define('A.view.main.company.mobile.Summary', {
             items: [
                 {
                     xtype: 'component',
-                    padding: 15,
+                    padding: '18 0 15 0',
                     plugins: 'responsive',
                     responsiveConfig: {
                         width: {
@@ -57,7 +57,7 @@ Ext.define('A.view.main.company.mobile.Summary', {
             },
             bind: {
                 html:
-                    '<div class="property"><div class="title">Рейтинг</div> <div class="value">{rating}</div></div>' +
+                    '<div class="property"><div class="title">Рейтинг</div> <div class="value">{formatRating}</div></div>' +
                     '<div class="property"><div class="title">Телефон</div> <div class="value">{phone}</div></div>' +
                     '<div class="property"><div class="title">Сайт</div> <div class="value">{site}</div></div>' +
                     '<div class="property"><div class="title">Почта</div> <div class="value">{email}</div></div>' +
