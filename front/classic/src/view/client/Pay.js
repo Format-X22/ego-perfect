4de12744233310bcb1f085c0b6be4a1a10104f12
Pavlov <oleg.pav.m@gmail.com>
2016-05-09 16:49:31 +0300

    Заготовка для оплаты.

--- front/classic/src/view/client/Pay.js
+++ front/classic/src/view/client/Pay.js
@@ -3,6 +3,7 @@
  */
 Ext.define('A.view.client.Pay', {
     extend: 'Ext.form.Panel',
+    controller: 'clientPay',
     xtype: 'clientPay',
 
     layout: {
@@ -12,7 +13,12 @@ Ext.define('A.view.client.Pay', {
     items: [
         {
             xtype: 'adminTopDescription',
-            html: 'Вкладка оплаты услуг сервиса.'
+            html:
+                'Оплата услуг сервиса.<br>' +
+                'Поддерживаются любые карты Visa и MasterCard.<br>' +
+                'Уровень безопасности используемого платежного сервиса подтвержден сертификатом PCI DSS.<br><br>' +
+                'При необходимости будет предоставлен акт выполненых работ.<br>' +
+                'Для получения акта напишите письмо на адрес contact@фирмы.онлайн'
         },
         {
             name: 'payDate',
@@ -34,21 +40,57 @@ Ext.define('A.view.client.Pay', {
         {
             xtype: 'container',
             width: '100%',
-            height: 200,
             layout: {
-                type: 'hbox',
+                type: 'vbox',
                 align: 'center',
                 pack: 'center'
             },
+            defaults: {
+                width: 450,
+                margin: 20
+            },
             items: [
                 {
+                    xtype: 'image',
+                    width: 120,
+                    height: 75,
+                    margin: '0 0 10 0',
+                    src: '/resources/img/logo.svg',
+                    plugins: 'responsive',
+                    responsiveConfig: {
+                        'height > 600': {
+                            hidden: false
+                        },
+                        'height < 600': {
+                            hidden: true
+                        }
+                    }
+                },
+                {
+                    xtype: 'button',
+                    text: 'Оплатить на 1 месяц - 2 190 рублей',
+                    handler: 'pay1Month'
+                },
+                {
+                    xtype: 'button',
+                    text: 'Оплатить на 12 месяцев - 26 280 рублей',
+                    handler: 'pay12Month'
+                },
+                {
                     xtype: 'component',
-                    html: '<b>Платные услуги будут доступны после 11 мая.</b>'
+                    html: 
+                        '<div style="' +
+                            'width: 450px;' +
+                            'text-align: center;' +
+                            'font-style: italic;' +
+                        '">' +
+                            'При оплате на 12 месяцев +2 месяца бесплатно.' +
+                        '</div>'
                 }
             ]
         },
         {
-            flex: 3
+            flex: 4
         }
     ]
 });
