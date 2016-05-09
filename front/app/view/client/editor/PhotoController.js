e93a9e0b7a90e8abb17e86bfc2afcb8ca6b91f75
Pavlov <oleg.pav.m@gmail.com>
2016-05-09 23:03:20 +0300

    "Загружено!" для полей фото редактора

--- front/app/view/client/editor/PhotoController.js
+++ front/app/view/client/editor/PhotoController.js
@@ -5,5 +5,18 @@ Ext.define('A.view.client.editor.PhotoController', {
     extend: 'A.view.widget.AbstractSaveToolbarController',
     alias: 'controller.clientEditorPhoto',
 
-    url: '/api/client/photo'
+    url: '/api/client/photo',
+
+    /**
+     * @inheritdoc
+     */
+    successSaveHandler: function () {
+        var fileFields = this.getView().query('filefield');
+
+        Ext.each(fileFields, function (field) {
+            field.setRawValue('Загружено успешно!');
+        }, this);
+
+        this.callParent(arguments);
+    }
 });
