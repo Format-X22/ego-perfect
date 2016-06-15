8dec34d083e7c279ed3b4642627e5aa112ca2e84
Pavlov <oleg.pav.m@gmail.com>
2016-06-15 03:39:24 +0300

    Сендер и абстрактное письмо [1]

--- mail/AbstractMail.js
+++ mail/AbstractMail.js
@@ -2,5 +2,31 @@
  * Абстрактный класс письма.
  */
 Ext.define('B.mail.AbstractMail', {
-    //
+
+    mixins: [
+        'B.mixin.AsyncResult'
+    ],
+
+    config: {
+
+        /**
+         * @cfg {String} signature Универсальная подпись для писем с контактами.
+         */
+        signature: [
+            '<hr>' +
+            'ООО "Простые числа"',
+            '<a href="http://фирмы.онлайн/">http://фирмы.онлайн</a>',
+            '<a href="mailto:w@фирмы.онлайн">w@фирмы.онлайн</a>',
+            '+7 (925) 154-68-79'
+        ].join('<br><br>')
+    },
+
+    constructor: function (config) {
+        this.initConfig(
+            Ext.apply(
+                Ext.clone(this.config),
+                config
+            )
+        );
+    }
 });
--- mail/Sender.js
+++ mail/Sender.js
@@ -2,5 +2,27 @@
  * Драйвер для отправки почты.
  */
 Ext.define('B.mail.Sender', {
-    singleton: true
+    singleton: true,
+
+    config: {
+        
+        /**
+         * @private
+         * @cfg {Object} sender Отправщик писем.
+         */
+        sender: require('sendgrid')(outerResourcesConfig.sendGrid.apiKey)
+    },
+    
+    constructor: function (config) {
+        this.initConfig(
+            Ext.apply(
+                Ext.clone(this.config),
+                config
+            )
+        );
+    },
+    
+    send: function (config, callback) {
+        
+    }
 });
