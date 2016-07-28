152d5716e7d6caec9130ab37e60795912e6b16cc
Pavlov <oleg.pav.m@gmail.com>
2016-07-28 15:36:24 +0300

    Классы писем с еженедельным отчетом и уведомлениями об окончании размещения.

---
+++ mail/WeekReport.js
@@ -0,0 +1,71 @@
+/**
+ * Письмо с еженедельным отчетом.
+ * Требуется заполнить все поля конфигурации.
+ */
+Ext.define('B.mail.WeekReport', {
+    extend: 'B.mail.AbstractMail',
+
+    subject: 'Недельный отчет Фирмы онлайн',
+    tpl:
+        'Здравствуйте!' +
+        '<br><br>' +
+        'За последнюю неделю:<br>' +
+        'Просмотров вашей компании - <b>{views}</b><br>' +
+        'Отзывов о вашей компании - <b>{reviews}</b><br>' +
+        'Средняя звездность отзывов за неделю - <b>{stars}</b><br>' +
+        'Рейтинг вашей компании увеличился на - <b>{rating}</b><br>' +
+        '<br><br>' +
+        'Прямая ссылка на вашу компанию - ' +
+        '<a href="http://firms-online.com/page-company-{id}">' +
+            '{name}' +
+        '</a>' +
+        '<br><br>' +
+        'Успехов!' +
+        '<br><br>',
+
+    config: {
+
+        /**
+         * @cfg {Number} views Просмотры.
+         */
+        views: 0,
+
+        /**
+         * @cfg {Number} reviews Отзывы.
+         */
+        reviews: 0,
+
+        /**
+         * @cfg {Number} stars Звездность.
+         */
+        stars: 0,
+
+        /**
+         * @cfg {Number} rating Рейтинг.
+         */
+        rating: 0,
+
+        /**
+         * @cfg {String} id Идентификатор компании.
+         */
+        id: '',
+
+        /**
+         * @cfg {String} name Имя компании.
+         */
+        name: ''
+    },
+
+    init: function () {
+        this.callParent(arguments);
+
+        this.setData({
+            views: this.getViews(),
+            reviews: this.getReviews(),
+            stars: this.getStars(),
+            rating: this.getRating(),
+            id: this.getId(),
+            name: this.getName()
+        });
+    }
+});
